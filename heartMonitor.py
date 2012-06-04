991eee48660f5e05de01c37776f2867f164dfc3b
Jonathan Heathcote <mail@jhnet.co.uk>
2012-06-05 00:33:28 +0100

    Cleaned up existing classes.

--- heartMonitor.py
+++ heartMonitor.py
@@ -9,24 +9,48 @@ from colorsys import rgb_to_hsv
 class Camera(object):
 	
 	def __init__(self, camera = 0):
+		"""
+		A simple web-cam wrapper.
+		"""
 		self.cam = cv.CaptureFromCAM(camera)
 	
 	def get_frame(self):
+		"""
+		Return the most recent image from the webcam
+		"""
 		return cv.QueryFrame(self.cam)
 
 
 class FaceDetector(object):
 	
 	def __init__(self, width, height, cascade_file="haarcascade_frontalface_alt.xml"):
+		"""
+		Detects faces in an image.
+		@param width        Width of the images that will be supplied
+		@param height       Height of the images that will be supplied
+		@param cascade_file Haar cascade data file for fronts of faces
+		"""
+		
+		# Load the cascade
 		self.cascade = cv.Load(cascade_file)
+		
+		# Storage for the algorithm to use
 		self.storage = cv.CreateMemStorage()
 		
+		# A grayscale buffer to copy images for processing into
 		self.gray = cv.CreateImage((width, height), 8, 1)
 	
 	def get_faces(self, image):
+		"""
+		Given an opencv image, return a ((x,y,w,h), certainty) tuple for each face
+		detected.
+		"""
+		
+		# Convert the image to grayscale and normalise
 		cv.CvtColor(image, self.gray, cv.CV_BGR2GRAY)
 		cv.EqualizeHist(self.gray, self.gray)
 		
+		# Detect faces
 		return cv.HaarDetectObjects(self.gray, self.cascade, self.storage,
 		                            scale_factor = 1.3,
 		                            min_neighbors = 2,
@@ -34,76 +58,124 @@ class FaceDetector(object):
 		                            min_size = (40,40))
 	
 	def get_best_face(self, image):
-		faces = self.get_faces(image)
-		best = None
-		for face in faces:
-			if best is None or face[1] > best[1]:
-				best = face
-		
-		return best
+		"""
+		Wrapper around get_faces which returns the face with the highest certainty
+		or None if no faces were found
+		"""
+		try:
+			return max(self.get_faces(image),
+			           key = (lambda f: f[1]))
+		except ValueError:
+			return None
 
 
 class HeartMonitor(object):
 	
-	def __init__(self, window_size, fps = 30, min_bpm = 50, max_bpm = 200):
-		self.num_samples = int(window_size*fps)
-		self.fps = fps
+	def __init__(self, window_duration, fps = 30, min_bpm = 50, max_bpm = 200):
+		"""
+		Class which detects heart-beats in a sequence of image colour samples.
+		@param window_duration The number of seconds of samples to use
+		@param fps             The nominal sample rate
+		@param min_bpm         Minimum cut-off for possible heartrates
+		@param max_bpm         Maximum cut-off for possible heartrates
+		"""
+		
 		self.min_bpm = min_bpm
 		self.max_bpm = max_bpm
 		
-		self.time = []
-		self.data = []
+		# The maximum number of samples to buffer
+		self.buf_size = int(window_duration*fps)
 		
-		self.bins = []
-	
-	
-	def get_bpm(self):
-		interpolated = numpy.interp(
-		                 numpy.linspace(self.time[0], self.time[-1],
-		                                len(self.time)),
-		                 self.time,
-		                 self.data)
-		fft = numpy.abs(numpy.fft.rfft(interpolated))
-		
-		fps = float(len(self.time)) / (self.time[-1] - self.time[0])
-		
-		# Number of bins in the full FFT
-		bins = len(self.data)
-		
-		def bin_to_bpm(bin):
-			return (60.0 * bin * fps) / float(bins)
-		# Calculate the BPM of this bin
-		
-		f = open("fft","w")
-		for bin in range(len(fft)):
-			f.write("%s %s\n"%(bin_to_bpm(bin+1), fft[bin]))
-		
-		# Get the index of the min and max bins to scan
-		min_bin = int(float(bins * self.min_bpm) / float(60.0 * fps))
-		max_bin = int(float(bins * self.max_bpm) / float(60.0 * fps))
-		
-		# Find the bin with the highest intensity
-		best_bins = sorted(range(min_bin, max_bin),
-		                   key=(lambda i: fft[i-1]),
-		                   reverse=True)
-		
-		# Store the calucated bins
-		self.bins = fft[min_bin-1:max_bin]
-		
-		return bin_to_bpm(best_bins[0])
+		# Buffer of (timestamp, value) tuples
+		self.buf = []
 	
 	
 	@property
-	def full(self):
-		return len(self.data) >= self.num_samples
+	def fps(self):
+		"""
+		The average framerate/samplerate of the buffer
+		"""
+		return float(len(self.buf)) / (self.buf[-1][0] - self.buf[0][0])
 	
 	
-	def add(self, time, datum):
-		if self.full:
-			self.data.pop(0)
-			self.time.pop(0)
-		self.time.append(time)
-		self.data.append(datum)
+	def get_fft(self):
+		"""
+		Perform an Fast-Fourier-Transform on the buffer and return the magnitude of
+		the bins.
+		"""
+		# Get the "ideal" evenly spaced times
+		even_times = numpy.linspace(self.buf[0][0], self.buf[-1][0], len(self.buf))
+		
+		# Interpolate the data to generate evenly temporally spaced samples
+		interpolated = numpy.interp(even_times, *zip(*self.buf))
+		
+		# Perform the FFT
+		return numpy.abs(numpy.fft.rfft(interpolated))
+	
+	
+	def bin_to_bpm(self, bin):
+		"""
+		Convert an FFT bin number into a heart-rate in beats-per-minute for the
+		current framerate. Bin numbers start from 1.
+		"""
+		
+		return (60.0 * bin * self.fps) / float(len(self.buf))
+	
+	
+	def bpm_to_bin(self, bpm):
+		"""
+		Convert a heart-rate in beats-per-minute into an FFT bin number for the
+		current framerate. Bin numbers start from 1.
+		"""
+		
+		return int(float(len(self.buf) * bpm) / float(60.0 * self.fps))
+	
+	
+	def get_bpm(self):
+		"""
+		Get the current beats-per-minute and the band of FFT data within the allowed
+		heart-rate range as a list of (bpm, amplitude) tuples.
+		"""
+		
+		fft = self.get_fft()
+		
+		# Get the bin numbers of the bounds of the possible allowed heart-rates in
+		# the FFT
+		min_bin = self.bpm_to_bin(self.min_bpm)
+		max_bin = self.bpm_to_bin(self.max_bpm)
+		
+		# Find the bin with the highest intensity (the heartbeat)
+		best_bin = max(range(min_bin, max_bin),
+		               key=(lambda i: fft[i-1]))
+		heartrate = self.bin_to_bpm(best_bin)
+		
+		# Produce the FFT data in the format described above
+		fft_data = zip((self.bin_to_bpm(b) for b in range(min_bin, max_bin+1)),
+		               fft[min_bin-1:max_bin])
+		
+		return heartrate, fft_data
+	
+	
+	@property
+	def buf_full(self):
+		return len(self.buf) >= self.buf_size
+	
+	
+	def add_sample(self, time, value):
+		"""
+		Add a new colour sample
+		"""
+		if self.buf_full:
+			self.buf.pop(0)
+		
+		self.buf.append((time, value))
+	
+	
+	def reset(self):
+		"""
+		Reset the heartrate monitor, start from scratch again.
+		"""
+		self.buf = []
 
 
 cam = Camera(0)
@@ -149,7 +221,7 @@ while True:
 		y += h/4
 		
 		cv.SetImageROI(frame, (x,y,w,h))
-		hm.add(time.time(), cv.Avg(frame)[1])
+		hm.add_sample(time.time(), cv.Avg(frame)[1])
 		cv.ResetImageROI(frame)
 		if show_face:
 			cv.Rectangle(frame, tuple(map(int, (x,y))), tuple(map(int, (x+w, y+h))), (0,255,0))
@@ -159,11 +231,12 @@ while True:
 		
 		if ffts:
 			if show_fft:
-				colour = (0,255,0) if hm.full else (0,0,255)
+				colour = (0,255,0) if hm.buf_full else (0,0,255)
 				cv.PolyLine(frame, [ffts], False, colour, 3)
 		
 		if i == 0:
-			bpmh.append(hm.get_bpm())
+			bpm, fft_data = hm.get_bpm()
+			bpmh.append(bpm)
 			if len(bpmh) > 100:
 				bpmh.pop(0)
 			bpm = sum(bpmh) / len(bpmh)
@@ -171,11 +244,12 @@ while True:
 			
 			i = 1
 			
-			if len(hm.bins) > 0:
+			if len(fft_data) > 0:
 				ffts = []
-				for num,value in enumerate(hm.bins):
-					ffts.append((int(640*(float(num)/len(hm.bins))), 
-					             240+(240-int(240*(float(value)/max(hm.bins))))))
+				for num,data in enumerate(fft_data):
+					_, value = data
+					ffts.append((int(640*(float(num)/len(fft_data))), 
+					             240+(240-int(240*(float(value)/max((d[1] for d in fft_data)))))))
 		i-= 1
 		
 		cv.ShowImage("w1", frame)
@@ -186,8 +260,7 @@ while True:
 	if key == 27:
 		break
 	elif key == ord("r"):
-		hm.data = []
-		hm.time = []
+		hm.reset()
 		ffts = []
 		i = 30
 		face = None
